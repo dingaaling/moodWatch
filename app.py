9f3c7a97352f620ba6f2f8518e6bf69738d86da6
Jennifer Ding <jending12@gmail.com>
2018-06-20 17:49:01 -0400

    new front end

--- app.py
+++ app.py
@@ -7,23 +7,26 @@ analyzer = SentimentIntensityAnalyzer()
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments, sentiment= [], []
+comments, moods = [], []
+baselineMood, sessionMood = 0.2, 0.0
 
 @app.route("/", methods=["GET", "POST"])
 def main():
 
+    sessionMood = np.mean(moods)
+
     #if GET: render website with comments
     if request.method == "GET":
-        return render_template("index.html", comments=comments, mood=sentiment)
+        return render_template("index.html", comments=comments, moods=moods, baselineMood=baselineMood, sessionMood=sessionMood)
 
     #if POST: add comment to list and calculate mood score
     comment = request.form["contents"]
     comments.append(comment)
 
     vs = analyzer.polarity_scores(comment)
-    sentiment.append(vs["compound"])
-    print(np.mean(sentiment))
+    moods.append(vs["compound"])
 
+    print("session mood is: ", sessionMood)
     return redirect(url_for("main"))
 
 if __name__ == "__main__":
