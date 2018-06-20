c8f519daa94a48404086a2e7e85129158f97e031
Jennifer Ding <jending12@gmail.com>
2018-06-20 11:53:58 -0400

    flask app accepts comments and prints sentiment

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+comments = []
 
+@app.route("/", methods=["GET", "POST"])
+def main():
+    #if GET: render website with comments
+    if request.method == "GET":
+        return render_template("index.html", comments=comments)
+    #if POST: extract new comments from textbox
+    comment = request.form["contents"]
+    vs = analyzer.polarity_scores(comment)
+    print(vs)
+    comments.append(comment)
+    return redirect(url_for("main"))
 
 if __name__ == "__main__":
     app.run()
